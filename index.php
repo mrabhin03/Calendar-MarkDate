1b55bd8048f9cfc11ca4f7d37fbb23b0abc1fd93
Abhin M <abhinm24@gmail.com>
2025-01-28 21:01:52 +0530

    Critical Change

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>Activities</title>
     <link rel="stylesheet" href="style.css?v=<?=time()?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="Assets/Logo192x192.png">
+    <link rel="manifest" href="manifest.json">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
     <script src="script.js?v=<?=time()?>" defer></script>
